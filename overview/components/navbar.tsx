64ed6866428161fb29b245d75d920bf95dbb7caf
Yannick Stritzke <yannick-stritzke@outlook.de>
2023-03-10 11:19:10 +0100

    Neues Update
    neues Update

---
+++ overview/components/navbar.tsx
@@ -0,0 +1,18 @@
+const NavBar = () =>{
+
+    return  <>
+<nav className="navbar navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand">Serlo Overview 2</a>
+    <form className="d-flex">
+      <input className="form-control me-2" type="search" placeholder="Suche" aria-label="Search"/>
+      <button className="btn btn-outline-success" type="submit">Suche</button>
+    </form>
+  </div>
+</nav>
+
+</>
+    
+    }
+    
+    export default NavBar
---
+++ overview/components/table1.tsx
@@ -0,0 +1,58 @@
+
+const Table1 = () => {
+
+       
+   
+    return <>
+
+  
+
+
+    <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Vorname</th>
+      <th scope="col">Nachname</th>
+      <th scope="col">Kontakt</th>
+      <th scope="col">Unit</th>
+      <th scope="col">Sherpa</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Anne</td>
+      <td>Reif</td>
+      <td>anne@serlo.org</td>
+      <td>U - Einbindung</td>
+      <td>Katharina Radstorfer</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Carolin</td>
+      <td>Jaser</td>
+      <td>carolin@serlo.org</td>
+      <td>P-Commenting System</td>
+      <td>Jonas Kleinnholz</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      
+      <td>Claudia</td>
+      <td>Bischoff</td>
+      <td>claudia@serlo.org</td>
+      <td>U - Begleitung von Autor*innen</td>
+      <td>Nishanth Chandrakumara</td>
+    </tr>
+  </tbody>
+</table>
+    
+    
+    </>
+    
+    
+    
+}
+ 
+export default Table1;
